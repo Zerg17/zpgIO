ca5acd161f9f0a2a2e65ecd8048fe1eb818cd8b2
Zerg17 <zerg1703@yandex.ru>
2024-04-07 20:19:50 +0300

    Rework json

--- main.go
+++ main.go
@@ -9,27 +9,33 @@ import (
 	"time"
 )
 
-const WORLD_SIZE = 64
-var world [WORLD_SIZE][WORLD_SIZE]int
+const WORLD_SIZE = 1
+const CHUNK_SIZE = 64
+
+type block_t struct {
+    Type int
+    Temp float32
+}
 
 type chunk_t struct {
     X, Y int
     Size int
-    Blocks [WORLD_SIZE][WORLD_SIZE]int
-    Temp [WORLD_SIZE][WORLD_SIZE]float32
+    Blocks [CHUNK_SIZE][CHUNK_SIZE]block_t
 }
+
+var world [WORLD_SIZE][WORLD_SIZE]chunk_t
 
 func generateWorld() {
     for i := 0; i < WORLD_SIZE; i++ {
         for j := 0; j < WORLD_SIZE; j++ {
-        world[i][j] = rand.Intn(4)
+            world[0][0].Blocks[i][j].Type = rand.Intn(4)
         }
     }
 }
 
 func worldProc() {
     for {
-        world[rand.Intn(WORLD_SIZE)][rand.Intn(WORLD_SIZE)] = 0
+        world[0][0].Blocks[rand.Intn(WORLD_SIZE)][rand.Intn(WORLD_SIZE)].Type = 0
         time.Sleep(500 * time.Millisecond)
     }
 }
@@ -62,15 +68,8 @@ func handlerStatic(w http.ResponseWriter, r *http.Request) {
 
 func handlerJson(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r.URL.Path)
-    if r.URL.Path == "/json/map" {
-        chunk := chunk_t{
-            X: 0,
-            Y: 0,
-            Size: WORLD_SIZE,
-            Blocks: world,
-            Temp: [64][64]float32{},
-        }
-        jsonData, err := json.Marshal(chunk)
+    if r.URL.Path == "/json/chunk" {
+        jsonData, err := json.Marshal(world[0][0])
         if err != nil {
             fmt.Fprintf(w, "Error: %v", err)
         } else {
