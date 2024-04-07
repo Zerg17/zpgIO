34484d395c7b026e50992ad4713812af85931e41
Dmitry Borisov <mitya.borisov@yandex.ru>
2024-04-07 05:49:54 +0500

    Fix codestyle (#1)

--- main.js
+++ main.js
@@ -2,20 +2,20 @@ let canvas = document.getElementById('canvas')
 
 let ctx  = canvas.getContext('2d')
 
-ctx.canvas.width  = ctx.canvas.parentNode.clientWidth*window.devicePixelRatio;
-ctx.canvas.height = ctx.canvas.parentNode.clientHeight*window.devicePixelRatio;
-w = ctx.canvas.width/window.devicePixelRatio
-h = ctx.canvas.height/window.devicePixelRatio
+ctx.canvas.width  = ctx.canvas.parentNode.clientWidth * window.devicePixelRatio
+ctx.canvas.height = ctx.canvas.parentNode.clientHeight * window.devicePixelRatio
+let w = ctx.canvas.width / window.devicePixelRatio
+let h = ctx.canvas.height / window.devicePixelRatio
 ctx.scale(window.devicePixelRatio, window.devicePixelRatio)
 
 ctx.strokeStyle = "#FFFA"
 ctx.fillStyle = "#FFFA"
 ctx.lineWidth = 0.5
 
-let drawLine = (ctx,x1,y1,x2,y2) => {
+let drawLine = (ctx, x1, y1, x2, y2) => {
     ctx.beginPath()
-    ctx.moveTo(x1,y1)
-    ctx.lineTo(x2,y2)
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
     ctx.stroke()
 }
 
